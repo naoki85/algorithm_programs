8e0354bf23e992b76a0310adaa9e179fcfdfc1e7
naoki85 <naoki.yoneyama.85@gmail.com>
2021-03-17 22:30:23 +0900

    Use for loop in Lcs

--- src/lcs/lcs.py
+++ src/lcs/lcs.py
@@ -2,30 +2,21 @@ class Lcs(object):
     def __init__(self, string, other):
         self.__string = string
         self.__other = other
-        self.__cache = [[-1 for _ in range(len(self.__other) + 1)] for _ in range(len(self.__string) + 1)]
+        self.__cache = [[0 for _ in range(len(self.__other) + 1)] for _ in range(len(self.__string) + 1)]
 
     def calc(self):
         len_string = len(self.__string)
         len_other = len(self.__other)
-        return self.__recursive_calc(len_string, len_other)
 
-    def __recursive_calc(self, x, y):
-        if x == 0 and y == 0:
-            return 0
+        for x in range(len_string + 1):
+            for y in range(len_other + 1):
+                if x > 0:
+                    self.__cache[x][y] = max(self.__cache[x][y], self.__cache[x - 1][y])
 
-        if self.__cache[x][y] != -1:
-            return self.__cache[x][y]
+                if y > 0:
+                    self.__cache[x][y] = max(self.__cache[x][y], self.__cache[x][y - 1])
 
-        res = 0
-        if x > 0:
-            res = max(res, self.__recursive_calc(x - 1, y))
+                if x > 0 and y > 0 and self.__string[x - 1] == self.__other[y - 1]:
+                    self.__cache[x][y] = max(self.__cache[x][y], self.__cache[x - 1][y - 1] + 1)
 
-        if y > 0:
-            res = max(res, self.__recursive_calc(x, y - 1))
-
-        if x > 0 and y > 0 and self.__string[x - 1] == self.__other[y - 1]:
-            res = max(res, self.__recursive_calc(x - 1, y - 1) + 1)
-
-        self.__cache[x][y] = res
-        return res
-
+        return self.__cache[len_string][len_other]
